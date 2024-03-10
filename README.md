18a17991eccb292cdd6d403a91e94cd5c14b82fb
mrzaizai2k <chibao24.12.1999@gmail.com>
2024-03-10 16:34:33 +0700

    first commit

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Note: [Source code is here](https://github.com/spacekitcat/cooley-tukey-fast-fourier-transform/blob/master/src/fourier_exp_000/fft.py), sorry about all the boiler plate, I used some project generator and shouldn't have!
 
+Note2: I used memoization to decrease the speed FFT. Big thanks to the original repo
+
 Fourier transforms are really cool and I want to learn the maths and concepts behind it all. This continues from [fourier-exp-01](https://github.com/spacekitcat/fourier-exp-01), a small C program that performs a temporal to frequency domain transform. This is a port of the C program, Python seems to have a better selection of libraries for parsing wave files, drawing graphs and doing maths.
 
 This project includes a Python implementation of the Discrete Fourier Transform (DFT) in the C project above, it also contains an
@@ -15,12 +17,12 @@ implementation of the Cooley-Tukey Fast Fourier Transform (FFT). The key differe
 git clone github.com/spacekitcat/<repository-name>
 cd <repository-name>
 python setup.py develop
-python src/cooley-tukey-fast-fourier-transform-implentation/fft.py resources/440hz.wav
+python src/fourier_exp_000/fft.py resources/440hz.wav
 ```
 
 The graph below shows the frequency graph for an input wave file made up of a single 440hz sine wave.
 
-![A graph with the frequencies from 0hz to 20050hz plotted along the x-axis (the frequency domain) and the magnitude plotted along the y-axis (the amplitude or magnitude, i.e. the contribution this frequency makes to the signal). The graph spikes at 440hz, showing 440hz as the dominant frequency](docs/440hz-example.png)
+![A graph with the frequencies from 0hz to 20050hz plotted along the x-axis (the frequency domain) and the magnitude plotted along the y-axis (the amplitude or magnitude, i.e. the contribution this frequency makes to the signal). The graph spikes at 440hz, showing 440hz as the dominant frequency](docs/new_result.new_result.jpg)
 
 The graph below shows the frequency graph for an input wave file made up of a 500hz and a 1200hz sine wave.
 
--- src/fourier_exp_000/fft.py
+++ src/fourier_exp_000/fft.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 import logging
 import time
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile as wav
@@ -51,12 +52,31 @@ def parse_args(args):
         const=logging.DEBUG)
     return parser.parse_args(args)
 
+
+def memoize(f):
+   cache = {}
+
+   def memoizedFunction(*args):
+      if args not in cache:
+         cache[args] = f(*args)
+      return cache[args]
+
+   memoizedFunction.cache = cache
+   return memoizedFunction
+
+@memoize
+def omega(k, size):
+    w = np.exp(-2j * np.pi * k / size)
+    return w
+
 def setup_logging(loglevel):
     logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
     logging.basicConfig(level=loglevel, stream=sys.stdout,
                         format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
 
-def my_fft(sample_set):
+def memoi_fft(sample_set):
+
+    # return freq_bins
     if len(sample_set) == 1:
         return sample_set
 
@@ -64,8 +84,33 @@ def my_fft(sample_set):
         print("{} must be a power of 2".format(len(sample_set)))
 
     size = len(sample_set)
-    even_fft = my_fft(sample_set[0::2])
-    odd_fft = my_fft(sample_set[1::2])
+    even_fft = memoi_fft(sample_set[0::2])
+    odd_fft = memoi_fft(sample_set[1::2])
+
+    freq_bins = [0.0] * size
+    for k in range(0, int(size / 2)):
+        freq_bins[k] = even_fft[k]
+        freq_bins[k + int(size / 2)] = odd_fft[k]
+
+    for k in range(0, int(size / 2)):
+        # e = np.exp(-2j * np.pi * k / size)
+        e = omega(k,size)
+        t = freq_bins[k]
+        freq_bins[k] = t + e * freq_bins[k + int(size / 2)]
+        freq_bins[k + int(size / 2)] = t - e * freq_bins[k + int(size / 2)]
+
+    return freq_bins
+
+def normal_fft(sample_set):
+    if len(sample_set) == 1:
+        return sample_set
+
+    if len(sample_set) % 2 != 0:
+        print("{} must be a power of 2".format(len(sample_set)))
+
+    size = len(sample_set)
+    even_fft = normal_fft(sample_set[0::2])
+    odd_fft = normal_fft(sample_set[1::2])
 
     freq_bins = [0.0] * size
     for k in range(0, int(size / 2)):
@@ -109,26 +154,40 @@ def main(args):
 
     print("Closing {}".format(args.input_file))
 
+    # Measure execution time and memory for normal_fft
     start = time.time()
-    freq_bins_my_fft = my_fft(sampleset)
+    freq_bins_normal_fft = normal_fft(sampleset)
     end = time.time()
-    my_fft_time = round(end - start, 2)
+    normal_fft_time = round(end - start, 5)
+    normal_fft_memory = sys.getsizeof(freq_bins_normal_fft)
 
+    # Measure execution time and memory for memoi_fft
+    start = time.time()
+    freq_bins_memoi_fft = memoi_fft(sampleset)
+    end = time.time()
+    memoi_fft_time = round(end - start, 5)
+    memoi_fft_memory = sys.getsizeof(freq_bins_memoi_fft)
+
+    print(f"normal_fft took {normal_fft_time} seconds and used {normal_fft_memory} bytes of memory")
+    print(f"memoi_fft took {memoi_fft_time} seconds and used {memoi_fft_memory} bytes of memory")
     start = time.time()
     freq_bins_numpy_fft = np.fft.fft(sampleset, window_size)
     end = time.time()
-    np_fft_time = round(end - start, 2)
+    np_fft_time = round(end - start, 5)
+    numpy_fft_memory = sys.getsizeof(freq_bins_numpy_fft)
+    print(f"numpy_fft took {np_fft_time} seconds and used {numpy_fft_memory} bytes of memory")
+
 
     start = time.time()
     freq_bins_my_dft = scale_for_graph(discrete_fourier_transform(sampleset, window_size), window_size)
     end = time.time()
-    my_dft_time = round(end - start, 2)
+    my_dft_time = round(end - start, 5)
 
-    fig, axs = plt.subplots(3, 1)
-    axs[0].set_title('My FFT (Cooley-Tukey) [time={}]'.format(my_fft_time))
+    fig, axs = plt.subplots(4, 1)
+    axs[0].set_title('My FFT (Cooley-Tukey) [time={}]'.format(normal_fft_time))
     axs[0].set_xlabel('Frequency (Hz)')
     axs[0].set_ylabel('Bin size')
-    axs[0].plot(np.arange(22050), scale_for_graph(freq_bins_my_fft[:len(freq_bins_my_fft) // 2], 22050))
+    axs[0].plot(np.arange(22050), scale_for_graph(freq_bins_normal_fft[:len(freq_bins_normal_fft) // 2], 22050))
     
     axs[1].set_title('Numpy FFT [time={}]'.format(np_fft_time))
     axs[1].set_xlabel('Frequency (Hz)')
@@ -140,7 +199,14 @@ def main(args):
     axs[2].set_ylabel('Bin size')
     axs[2].plot(np.arange(22050), scale_for_graph(freq_bins_my_dft[:len(freq_bins_numpy_fft) // 2], 22050))
 
+
+    axs[3].set_title('Normal FFT [time={}]'.format(memoi_fft_time))
+    axs[3].set_xlabel('Frequency (Hz)')
+    axs[3].set_ylabel('Bin size')
+    axs[3].plot(np.arange(22050), scale_for_graph(freq_bins_memoi_fft[:len(freq_bins_memoi_fft) // 2], 22050))
+
     fig.tight_layout()
+    plt.savefig('docs/new_result.jpg')
     plt.show()
 
 def run():
